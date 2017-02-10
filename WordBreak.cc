d466ae8d29fa925d20da81e7cb2552668df2f48c
TakefumiYamamura <yama1223xxx@gmail.com>
2017-02-10 19:57:13 +0900

    word break

---
+++ WordBreak.cc
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+	bool wordBreak(string s, vector<string>& wordDict){
+        vector<bool> dp = vector<bool>(s.size()+1, false);
+        dp[0] = true;
+        for (int i = 1; i <= s.size(); ++i){
+            if(dp[i-1] == false) continue;
+            for (int j = 0; j < wordDict.size(); ++j)
+            {
+                string cur = wordDict[j];
+                if(i + cur.size() - 1  > s.size()) continue;
+                if(s.substr(i-1, cur.size()) == cur){
+                    dp[i+cur.size() - 1] = true;
+                }
+            }
+        }
+        return dp[s.size()];
+	}
+};
