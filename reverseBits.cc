cb170031aa1d2363c41abc9142eb02f39d3811b9
TakefumiYamamura <yama1223xxx@gmail.com>
2017-02-27 19:43:48 +0900

    reverseBits.cc

--- reverseBits.cc
+++ reverseBits.cc
@@ -3,15 +3,14 @@ public:
     uint32_t reverseBits(uint32_t n) {
     	vector<int> bits(32, 0);
     	uint32_t tmp = n;
-    	int itr = 0;
-    	while(tmp){
+    	for (int i = 0; i < 32; ++i)
+    	{
     		if(tmp & 1){
-    			bits[itr] = 1;
-    		}else{
-    			bits[itr] = 0;
+    			bits[i] = 1;
+    		} else{
+    			bits[i] = 0;
     		}
-    		itr++;
-    		tmp = tmp >> 1;
+    		tmp >>= 1;
     	}
     	uint32_t ans = 0;
     	uint32_t tmp_two = 1; 
