c6692f674020a9a2f300e05405f0e85ca3c28ccf
TakefumiYamamura <yama1223xxx@gmail.com>
2017-02-28 14:52:02 +0900

    lexicographicalOrder.cc

---
+++ lexicographicalOrder.cc
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+    	vector<int> ans;
+    	if(n == 0)retrun ans;
+    	ans.push_back(1);
+    	int cur = 1;
+    	while(tmp <= n){
+    		tmp = tmp*10;
+    		
+
+
+    	}
+
+    	for (int i = 0; i < n; ++i)
+    	{
+    		
+    	}
+
+    	return ans;
+        
+    }
+};
