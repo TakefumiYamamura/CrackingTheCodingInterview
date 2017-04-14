47d6ef40f93d86db62df1a981fa77e9e8f106be8
TakefumiYamamura <yama1223xxx@gmail.com>
2017-04-14 19:17:28 +0900

    majority element

---
+++ majorityElement.cc
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int num = nums[0];
+        int count = 1;
+        for(int i = 1; i < nums.size(); i++){
+            if(count == 0) num = nums[i];
+            if(num != nums[i]){
+                count--;
+            }else{
+                count++;
+            }
+        }
+        return num;
+    }
+};
