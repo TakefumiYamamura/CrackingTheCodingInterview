4a1fea52840a9cbb8e6a470e60158b222f10cf33
TakefumiYamamura <yama1223xxx@gmail.com>
2017-05-03 18:43:25 +0900

    missing_numbers.cpp

---
+++ missing_numbers.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int ans = 0;
+        int size = nums.size();
+        for (int i = 0; i < size; ++i)
+        {
+            ans = ans ^ nums[i] ^ i;
+        }
+        return ans ^ size;
+    }
+};
