a1268471d83ea25fd16c5fb2da8d9c4291e81e9c
TakefumiYamamura <yama1223xxx@gmail.com>
2017-05-13 19:21:31 +0900

    mastermind.cpp

---
+++ mastermind.cpp
@@ -0,0 +1,39 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <unordered_set>
+#include <climits>
+#include <unordered_map>
+#include <string>
+#include <assert.h>
+using namespace std;
+
+void master_mind(string ans, string input){
+	assert(ans.length() == input.length());
+	unordered_set<char> ust;
+	int hit = 0;
+	int brow = 0;
+	for (int i = 0; i < ans.size(); ++i)
+	{
+		if(ans[i] == input[i]) hit++;
+		ust.inset(ans[i]);
+	}
+	for (int i = 0; i < ans.size(); ++i)
+	{
+		if(ans[i] != input[i] && ust.find(input[i]) != ust.end()) brow++;
+	}
+
+	cout << hit << " : " << brow << endl;
+
+
+}
+
+int main(){
+	vector<int> arr;
+	for (int i = 1; i <= 100; ++i)
+	{
+		if(i == 29 || i == 47) continue;
+		arr.push_back(i);
+	}
+	master_mind("RGBY", "GGRR");
+}
